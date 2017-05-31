8c2219f85bb9a26fa1dcffce18c84050d597e6cf
Issa Soumare <isoumare@atixnet.fr>
2017-05-31 12:07:07 +0200

    Add first call api source

---
+++ src/app/call-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
+
+@Injectable()
+export class CallApiService {
+    private newsUrl = 'http://newsapi.org/v1/sources';  // URL to web api
+    constructor(private http: Http) { }
+
+    getSourcesNews(): any {
+
+    return this.http.get(this.newsUrl)
+               .toPromise()
+               .then(response => response.json().data)
+               .catch(this.handleError);
+   }
+
+   private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+}
